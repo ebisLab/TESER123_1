06869d2bdf8bdf12503ae37ecf4b13a19e0acb31
ebisLab <ebaiden@techie.com>
2020-11-12 13:50:26 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import './index.css';
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
-||||||| merged common ancestors
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -31,4 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 39006cc929d8c5d7d8e363fd0657ba0110a7144e
